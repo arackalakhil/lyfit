e91a91233c8789173763bb5ccee5494cc61472da
arackalakhil <makil029@gamil.com>
2024-05-02 17:42:37 +0530

    Added new feature

---
+++ js/Google Sheet.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx2nwrBe8Ldvc9IVNV23fmpSk-X_uHH7aeNfYR8iYD_Oqc90bowK0MIDb51FzaIEyih/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! We’ll take a closer look."))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
---
+++ js/googlesheet.js
@@ -0,0 +1,10 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz9U6qdOD56VNhdfXz1_MpamUZmKYRbihOHRZS6bzCIRIS5XsnT1A_cAvBawJOvj3ggKg/exec'
+const form = document.forms['contact-form1']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! we will contact you soon" ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
