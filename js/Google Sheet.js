e48f53c3b3466f7ba5327fe560b8b8ffc7884688
arackalakhil <makil029@gamil.com>
2024-05-02 18:38:47 +0530

    Added new feature2

--- js/Google Sheet.js
+++ js/Google Sheet.js
@@ -1,11 +1,20 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbx2nwrBe8Ldvc9IVNV23fmpSk-X_uHH7aeNfYR8iYD_Oqc90bowK0MIDb51FzaIEyih/exec'
 
-const form = document.forms['contact-form']
+const form = document.forms['contact-form'];
+const loadingOverlay = document.getElementById('loading-overlay');
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! We will contact you soon."))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+    e.preventDefault();
+    loadingOverlay.style.display = 'block'; // Show loading overlay
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+        loadingOverlay.style.display = 'none'; // Hide loading overlay
+        alert("Thank you! We’ll take a closer look.");
+        window.location.reload();
+    })
+    .catch(error => {
+        loadingOverlay.style.display = 'none'; // Hide loading overlay
+        console.error('Error!', error.message);
+    });
+});
--- js/googlesheet.js
+++ js/googlesheet.js
@@ -1,10 +1,19 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz9U6qdOD56VNhdfXz1_MpamUZmKYRbihOHRZS6bzCIRIS5XsnT1A_cAvBawJOvj3ggKg/exec'
-const form = document.forms['contact-form1']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz9U6qdOD56VNhdfXz1_MpamUZmKYRbihOHRZS6bzCIRIS5XsnT1A_cAvBawJOvj3ggKg/exec';
+const form = document.forms['contact-form1'];
+const loadingOverlay = document.getElementById('loading-overlay');
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! We’ll take a closer look." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+    e.preventDefault();
+    loadingOverlay.style.display = 'block'; // Show loading overlay
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+        loadingOverlay.style.display = 'none'; // Hide loading overlay
+        alert("Thank you! We’ll take a closer look.");
+        window.location.reload();
+    })
+    .catch(error => {
+        loadingOverlay.style.display = 'none'; // Hide loading overlay
+        console.error('Error!', error.message);
+    });
+});
